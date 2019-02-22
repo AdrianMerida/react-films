1d8832274d9c91853c4ae9be1ddba89f7cf49118
Carlos del Prado <carlos.delprado@11paths.com>
2019-02-22 21:56:12 +0100

    feat: film rating & genres

--- src/components/films/FilmList.js
+++ src/components/films/FilmList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import FilmItem from './FilmItem';
 
 export default class FilmList extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
