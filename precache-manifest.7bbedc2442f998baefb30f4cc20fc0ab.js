dab1c5d4f6c5d78bc8aefb604d515767dab33d47
Carlos del Prado <carlos.delprado@11paths.com>
2019-02-23 08:54:20 +0100

    Updates

---
+++ precache-manifest.7bbedc2442f998baefb30f4cc20fc0ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "20661276f88ce3b71b5e",
+    "url": "/react-films/static/css/main.288d17e7.chunk.css"
+  },
+  {
+    "revision": "20661276f88ce3b71b5e",
+    "url": "/react-films/static/js/main.20661276.chunk.js"
+  },
+  {
+    "revision": "1ef408d7a1e4c1256690",
+    "url": "/react-films/static/js/runtime~main.1ef408d7.js"
+  },
+  {
+    "revision": "caec6d2172d49e01d2aa",
+    "url": "/react-films/static/css/2.fe0ed36c.chunk.css"
+  },
+  {
+    "revision": "caec6d2172d49e01d2aa",
+    "url": "/react-films/static/js/2.caec6d21.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-films/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-films/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-films/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-films/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-films/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "29d665623a54e6dc438b360b636061b5",
+    "url": "/react-films/index.html"
+  }
+];
