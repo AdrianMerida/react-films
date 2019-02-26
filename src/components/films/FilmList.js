ea34bfda3050f802346ab1bd8232c525f59007f3
Riftland <molpe@mbp-de-fco.home>
2019-02-26 19:03:07 +0100

    refactor: extracted some function and changed the films source

--- src/components/films/FilmList.js
+++ src/components/films/FilmList.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import FilmItem from './FilmItem';
+import React, { Component } from 'react'
+import FilmItem from './FilmItem'
+import films from '../../data/films'
 
-export default class FilmList extends Component {
-  
+class FilmList extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      films: this.props.films || []
+      films: [...films] || []
     }
   }
 
-  onClickDeleteFilm = (film) => {
+  onClickDeleteFilm = film => {
     this.setState({
       films: this.state.films.filter(f => f !== film)
     })
   }
 
+  listFilms = () =>
+    this.state.films.map((film, index) => (
+      <FilmItem
+        key={index}
+        {...film}
+        onClickDelete={this.onClickDeleteFilm.bind(this, film)}
+      />
+    ))
+
   render() {
-    return (
-      <div className="card-columns">
-        {this.state.films.map((film, index) => {
-          return <FilmItem key={index} {...film} onClickDelete={this.onClickDeleteFilm.bind(this, film)}/>
-        })}
-      </div>
-    );
+    return <div className="card-columns">{this.listFilms()}</div>
   }
 }
+
+export default FilmList
